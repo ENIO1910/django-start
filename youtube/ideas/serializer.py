5e282be66070c74ab9617ab17893553527781d64
ENIO1910 <patrykstaniewski1910@gmail.com>
2025-03-18 21:24:52 +0100

    feat: Implement RESTful API for Idea and Vote models with serializers and viewsets

---
+++ youtube/ideas/serializer.py
@@ -0,0 +1,12 @@
+from .models import Idea, Vote
+from rest_framework import serializers
+
+class IdeaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Idea
+        fields = '__all__'
+
+class VoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ['idea', 'reason']
--- youtube/youtube/settings.py
+++ youtube/youtube/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ideas.apps.IdeasConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

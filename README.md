1d3f13dcbc2431dd2391a435f948b108b68b46c1
ENIO1910 <patrykstaniewski1910@gmail.com>
2025-03-22 10:20:21 +0100

    feat: Configure PostgreSQL database and add Docker support with docker-compose

---
+++ README.md
@@ -0,0 +1,7 @@
+# DEV
+
+1. List of all containers `` docker ps``
+2. Enter to container ``docker exec -it <container_id> bash``
+3. To rebuild the container ``docker-compose build <container_name>``
+4. To start containers ``docker-compose up -d``
+5. To stop containers ``docker-compose down``
--- youtube/youtube/settings.py
+++ youtube/youtube/settings.py
@@ -77,13 +77,16 @@ WSGI_APPLICATION = 'youtube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
-
-# Password validation
+        # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
